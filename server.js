a777c5e9c46017283d182ff0b303a57807a17e17
Josmar Salvador <josmarmp96@gmail.com>
2021-09-02 16:31:55 -0500

    En Corto Backend Server in NodeJs Initialized

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+server.listen( 3000, '192.168.0.17' || 'localhost', function () {
+    console.log('NodeJs App ' + process.pid + ' Initialized');
+});
